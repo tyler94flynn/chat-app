fb427058c3c6d137a92fe3d354efe45fb5529134
Tyler Flynn <tyler94flynn@gmail.com>
2022-08-17 12:13:39 -0400

    uuid implemented & printed to top of page on create id button press

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,6 +1,9 @@
 import React, { useRef } from 'react'
 import { Container, Form, Button } from 'react-bootstrap'
+import { v4 as uuidv4 } from 'uuid'
 
+//dont understand useRef or this function yet, but it creates a unique id
+//using uuidv4 (and prints to {id} at top of page in app.js)
 export default function Login({ onIdSubmit }) {
     const idRef = useRef()
 
@@ -8,6 +11,10 @@ export default function Login({ onIdSubmit }) {
         e.preventDefault()
 
         onIdSubmit(idRef.current.value)
+    }
+
+    function createNewId() {
+        onIdSubmit(uuidv4())
     }
 
 
@@ -19,7 +26,7 @@ export default function Login({ onIdSubmit }) {
             <Form.Control type = 'text' ref = {idRef} required />
         </Form.Group>
         <Button type="submit" className="me-2">Login</Button>
-        <Button variant="secondary">Create a new Id</Button>
+        <Button onClick = {createNewId} variant="secondary">Create a new Id</Button>
        </Form>
 
     </Container>
