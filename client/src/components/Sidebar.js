637c302492bd85b141b0eeddea7b068323243aba
Tyler Flynn <tyler94flynn@gmail.com>
2022-08-30 16:21:38 -0400

    added sidebar 25:30

---
+++ client/src/components/Sidebar.js
@@ -0,0 +1,23 @@
+import { React, useState } from 'react'
+import { Tab, Nav } from 'react-bootstrap'
+
+const CONVERSATIONS_KEY = 'conversations'
+const CONTACTS_KEY = 'contacts'
+
+export default function Sidebar({ id }) {
+  const [activeKey, setActiveKey] = useState(CONVERSATIONS_KEY)
+  return (
+    <div style={{ width: '250px' }} className='d-flex flex-column'>
+        <Tab.Container activeKey={activeKey} onSelect ={setActiveKey}> 
+            <Nav variant = 'tabs' className='justify-content-center'> 
+                <Nav.Item>
+                    <Nav.Link eventKey={CONVERSATIONS_KEY}>Conversations</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link eventKey={CONTACTS_KEY}>Contacts</Nav.Link>
+                </Nav.Item>
+            </Nav>
+        </Tab.Container>
+    </div>
+  )
+}
