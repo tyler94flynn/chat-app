b095941773a4709d6dacf9b161336d707dc8da62
Tyler Flynn <tyler94flynn@gmail.com>
2022-08-31 11:19:26 -0400

    contact modal 34:20

--- client/src/components/NewContactModal.js
+++ client/src/components/NewContactModal.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import { Modal } from 'react-bootstrap'
 
 export default function NewContactModal() {
-  return (
-    <div>NewContactModal</div>
-  )
+    return (
+        <>
+            <Modal.Header closeButton>
+                Create Contact
+            </Modal.Header>
+        </>
+    )
 }
